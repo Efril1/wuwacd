d3ceaac11bddb0649d3c3fe18bf32cdc1c295711
Efril1 <ccenkcc@gmail.com>
2025-08-03 17:21:50 +0300

    ads

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,14 @@
-<script setup lang="ts">
+<script setup>
+useHead({
+  script: [
+    {
+      src: '//pl27333916.profitableratecpm.com/1a/27/b2/1a27b26c8e8466dab313a1102cd2b5df.js',
+      async: true,
+      body: true
+    }
+  ]
+})
 </script>
-
 <template>
   <main class="relative h-screen">
     <div class="h-full w-full overflow-y-auto">
