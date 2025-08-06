acf34bd5e3cab2ba388471ea2b7c94e7e0cef697
Efril1 <ccenkcc@gmail.com>
2025-08-06 18:11:19 +0300

    changed char change button

--- app/components/countdown.vue
+++ app/components/countdown.vue
@@ -1,212 +1,247 @@
 <script setup lang="ts">
-import type { TabsItem } from '@nuxt/ui'
-type ColorChoice = 'primary' | 'info' | 'secondary' | 'success' | 'warning' | 'error' | 'neutral';
-
+import type { TabsItem } from "@nuxt/ui";
+import FadeContent from "../../src/blocks/Animations/FadeContent/FadeContent.vue";
+import StarBorder from "~~/src/blocks/Animations/StarBorder/StarBorder.vue";
+import ShinyText from "~~/src/blocks/TextAnimations/ShinyText/ShinyText.vue";
+type ColorChoice =
+  | "primary"
+  | "info"
+  | "secondary"
+  | "success"
+  | "warning"
+  | "error"
+  | "neutral";
 
 const props = defineProps({
   targetTime: {
     type: Date,
-    required: true
+    required: true,
   },
   bannerchar: {
     type: String,
-    required: true
+    required: true,
   },
   infoImageDesktop: {
     type: String,
-    default: ''
+    default: "",
   },
   infoImageMobile: {
     type: String,
-    default: ''
+    default: "",
   },
   backgroundVideo: {
     type: String,
-    default: ''
+    default: "",
   },
   colorChoice: {
-    type: String as PropType<ColorChoice>,
-    default: 'primary'
+    type: String as PropType<ColorChoice>,
+    default: "primary",
   },
-    characterInfo: {
+  characterInfo: {
     type: Object as PropType<{
-      name: string
-      element: string
-      weapon: string
-      about: string
+      name: string;
+      element: string;
+      weapon: string;
+      about: string;
     }>,
     default: () => ({
-      name: 'Character Name',
-      element: 'Element',
-      weapon: 'Weapon Type',
-      about: '"quote"—Character'
-    })
-  }
-
-})
+      name: "Character Name",
+      element: "Element",
+      weapon: "Weapon Type",
+      about: '"quote"—Character',
+    }),
+  },
+});
 const items = ref<TabsItem[]>([
   {
-    label: 'countdown',
-    value: 'countdown'
+    label: "countdown",
+    value: "countdown",
   },
   {
-    label: 'info',
-    value: 'info'
-  }
-])
+    label: "info",
+    value: "info",
+  },
+]);
+
+const now = ref(new Date());
+const timer = ref<NodeJS.Timeout | null>(null);
+const activeTab = ref("countdown");
+const route = useRoute();
+const isFirstPage = computed(() => route.path === '/first');
 
 
-const now = ref(new Date())
-const timer = ref<NodeJS.Timeout | null>(null)
-const activeTab = ref('countdown')
 
 const timeLeft = computed(() => {
-  const diff = props.targetTime.getTime() - now.value.getTime()
-  
-  if (diff <= 0) return null
-  
+  const diff = props.targetTime.getTime() - now.value.getTime();
+
+  if (diff <= 0) return null;
+
   return {
     days: Math.floor(diff / (1000 * 60 * 60 * 24)),
     hours: Math.floor((diff / (1000 * 60 * 60)) % 24),
     minutes: Math.floor((diff / (1000 * 60)) % 60),
     seconds: Math.floor((diff / 1000) % 60),
-  }
-})
+  };
+});
 
 const updateTime = () => {
-  now.value = new Date()
-}
+  now.value = new Date();
+};
 
 onMounted(() => {
-  updateTime()
-  timer.value = setInterval(updateTime, 1000)
-})
+  updateTime();
+  timer.value = setInterval(updateTime, 1000);
+});
 
 onBeforeUnmount(() => {
-  if (timer.value) clearInterval(timer.value)
-})
+  if (timer.value) clearInterval(timer.value);
+});
+
 </script>
 
 <template>
+  <div v-if="backgroundVideo" class="fixed inset-0 z-0">
+    <video autoplay muted loop playsinline class="w-full h-full object-cover">
+      <source :src="backgroundVideo" type="video/mp4" />
+    </video>
+    <div class="absolute inset-0 bg-black/30"></div>
+  </div>
 
-      <div v-if="backgroundVideo" class="fixed inset-0 z-0">
-      <video 
-        autoplay 
-        muted 
-        loop 
-        playsinline 
-        class="w-full h-full object-cover"
-      >
-        <source :src="backgroundVideo" type="video/mp4">
-      </video>
-<div class="absolute inset-0 bg-black/30"></div>   
- </div>
-
-    
   <div class="flex flex-col min-h-screen relative z-10">
     <!-- Tabs at top -->
     <div class="w-full max-w-2xl mx-auto pt-4">
-      <UTabs :color="colorChoice || 'primary'" v-model="activeTab" :items="items" />
+      <UTabs
+        :color="colorChoice || 'primary'"
+        v-model="activeTab"
+        :items="items"
+      />
     </div>
 
     <!-- Countdown Content -->
-     <div class="mx-auto">
-        <h2 class="chartext">{{ bannerchar }}</h2>
-      </div>
-    <div v-if="activeTab === 'countdown'" class="flex-1 grid place-items-center p-4">
+    <div class="mx-auto">
+      <NuxtLink :to="isFirstPage ? '/second' : '/first'">
+      <h2 class="chartext">{{ bannerchar }}</h2>
+      </NuxtLink>
+    </div>
+      <h2 class="mx-auto"> ↑ click to switch character</h2>
+    <div
+      v-if="activeTab === 'countdown'"
+      class="flex-1 grid place-items-center p-4"
+    >
       <div class="text-center">
-        
-        
-<div v-if="timeLeft" class="timer-display">
-  <div class="flex  gap-x-4 gap-y-2">
-    <div class="flex flex-col">
-      <div>
-      <span class="number text-4xl sm:text-5xl font-extrabold">{{ String(timeLeft.days).padStart(2, '0') }}</span>
-      </div>
-      <span class="text-sm">DAYS</span>
-      
-    </div>
-
-    <span class=" text-4xl  font-extrabold md:pt-4">:</span>
-
-    <div class="flex flex-col">
-      <span class="number text-4xl sm:text-5xl font-extrabold">{{ String(timeLeft.hours).padStart(2, '0') }}</span>
-      <span class="text-sm">HOURS</span>
-    </div>
-
-    <span class=" text-4xl  font-extrabold md:pt-4">:</span>
-
-    <div class="flex flex-col">
-      <span class="number text-4xl sm:text-5xl font-extrabold">{{ String(timeLeft.minutes).padStart(2, '0') }}</span>
-      <span class="text-sm">MINUTES</span>
-    </div>
-
-    <span class=" text-4xl  font-extrabold md:pt-4">:</span>
-
-    <div class="flex flex-col">
-      <span class="number text-4xl sm:text-5xl font-extrabold">{{ String(timeLeft.seconds).padStart(2, '0') }}</span>
-      <span class="text-sm">SECONDS</span>
-    </div>
-  </div>
-</div>
-        <slot v-else name="finished"></slot>
-      </div>
-    </div>
-    <div v-else-if="activeTab === 'info'" class="w-full max-w-2xl mx-auto">
-      <UCard class="overflow-hidden bg-black/20">
-        <div class="flex flex-col md:flex-row">
-          <!-- Left -->
-          <div class="w-full md:w-1/2 flex items-center justify-center">
-          <picture v-if="infoImageDesktop || infoImageMobile">
-            <source media="(max-width: 767px)" :srcset="infoImageMobile">
-            <img 
-              :src="infoImageDesktop" 
-              alt="Character Information" 
-              class="object-cover w-full h-64 md:h-full"
-            >
-          </picture>
-            <p v-else class="text-center p-4">No image provided</p>
-          </div>
-          
-          <!-- Right -->
-          <div class="w-full md:w-1/2 p-6 flex flex-col justify-center">
-            <div class="space-y-4 text-center md:text-left">
-              <h3 class="text-2xl font-bold">Character Info</h3>
-              <div class="grid grid-cols-2 gap-4">
-                <div>
-                  <p class="text-sm text-gray-500">Name</p>
-                  <p class="font-medium">{{ characterInfo.name }}</p>
-                </div>
-                <div>
-                  <p class="text-sm text-gray-500">Attribute</p>
-                  <p class="font-medium">{{ characterInfo.element }}</p>
-                </div>
-                <div>
-                  <p class="text-sm text-gray-500">Weapon</p>
-                  <p class="font-medium">{{ characterInfo.weapon }}</p>
-                </div>
+        <div v-if="timeLeft" class="timer-display">
+          <div class="flex flex-wrap gap-x-4 gap-y-2">
+            <div class="flex flex-col">
+              <div>
+                <span class="number text-4xl sm:text-5xl font-extrabold">{{
+                  String(timeLeft.days).padStart(2, "0")
+                }}</span>
               </div>
-             <p class="font-medium">{{ characterInfo.about }}</p>
+              <span class="text-sm">DAYS</span>
+            </div>
+
+            <span class="text-4xl font-extrabold md:pt-4">:</span>
+
+            <div class="flex flex-col">
+              <span class="number text-4xl sm:text-5xl font-extrabold">{{
+                String(timeLeft.hours).padStart(2, "0")
+              }}</span>
+              <span class="text-sm">HOURS</span>
+            </div>
+
+            <span class="text-4xl font-extrabold md:pt-4">:</span>
+
+            <div class="flex flex-col">
+              <span class="number text-4xl sm:text-5xl font-extrabold">{{
+                String(timeLeft.minutes).padStart(2, "0")
+              }}</span>
+              <span class="text-sm">MINUTES</span>
+            </div>
+
+            <span class="text-4xl font-extrabold md:pt-4">:</span>
+
+            <div class="flex flex-col">
+              <span class="number text-4xl sm:text-5xl font-extrabold">{{
+                String(timeLeft.seconds).padStart(2, "0")
+              }}</span>
+              <span class="text-sm">SECONDS</span>
             </div>
           </div>
         </div>
-      </UCard>
+        <slot v-else name="finished"></slot>
+      </div>
+    </div>
+
+    <div v-else-if="activeTab === 'info'" class="w-full max-w-2xl mx-auto">
+      <FadeContent
+        :blur="true"
+        :duration="500"
+        :delay="200"
+        :threshold="0.1"
+        :initial-opacity="0"
+        easing="ease-out"
+        class-name="my-fade-content"
+      >
+        <div class="content-to-fade">
+          <UCard class="overflow-hidden bg-black/20">
+            <div class="flex flex-col md:flex-row">
+              <!-- Left -->
+              <div class="w-full md:w-1/2 flex items-center justify-center">
+                <picture v-if="infoImageDesktop || infoImageMobile">
+                  <source
+                    media="(max-width: 767px)"
+                    :srcset="infoImageMobile"
+                  />
+                  <img
+                    :src="infoImageDesktop"
+                    alt="Character Information"
+                    class="object-cover w-full h-64 md:h-full"
+                  />
+                </picture>
+                <p v-else class="text-center p-4">No image provided</p>
+              </div>
+
+              <!-- Right -->
+              <div class="w-full md:w-1/2 p-6 flex flex-col justify-center">
+                <div class="space-y-4 text-center md:text-left">
+                  <h3 class="text-2xl font-bold">Character Info</h3>
+                  <div class="grid grid-cols-2 gap-4">
+                    <div>
+                      <p class="text-sm text-gray-500">Name</p>
+                      <p class="font-medium">{{ characterInfo.name }}</p>
+                    </div>
+                    <div>
+                      <p class="text-sm text-gray-500">Attribute</p>
+                      <p class="font-medium">{{ characterInfo.element }}</p>
+                    </div>
+                    <div>
+                      <p class="text-sm text-gray-500">Weapon</p>
+                      <p class="font-medium">{{ characterInfo.weapon }}</p>
+                    </div>
+                  </div>
+                  <p class="font-medium">{{ characterInfo.about }}</p>
+                </div>
+              </div>
+            </div>
+          </UCard>
+        </div>
+      </FadeContent>
     </div>
   </div>
-
 </template>
 
 <style scoped>
 @font-face {
-  font-family: 'Genshin';
-  src: url('/fonts/genshin.ttf') format('truetype');
+  font-family: "Genshin";
+  src: url("/fonts/genshin.ttf") format("truetype");
   font-weight: normal;
   font-style: normal;
   font-display: swap;
 }
 
 .timer-display {
-  font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;
+  font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas,
+    "Liberation Mono", "Courier New", monospace;
   margin-top: -100px;
 }
 
@@ -215,7 +250,6 @@ onBeforeUnmount(() => {
   font-weight: 800;
   line-height: 1;
 }
-
 
 @media (max-width: 768px) {
   .number {
@@ -226,23 +260,23 @@ onBeforeUnmount(() => {
   }
 }
 .chartext {
-    font-family: 'Genshin', sans-serif;
-    cursor: pointer;
-    color: #ffffff;
-    text-transform: uppercase;
-    padding: 15px;
-    z-index: 1;
-    animation: glow 1s ease-in-out infinite alternate;
-    font-size: 3rem;
+  font-family: "Genshin", sans-serif;
+  cursor: pointer;
+  color: #ffffff;
+  text-transform: uppercase;
+  padding: 15px;
+  z-index: 1;
+  animation: glow 1s ease-in-out infinite alternate;
+  font-size: 3rem;
 }
 @keyframes glow {
-    from {
-        text-shadow: 0 0 10px #fff, 0 0 3px #fff, 0 0 3px #ffffff, 0 0 3px #ffffff, 0 0 3px #ffffff, 0 0 3px #ffffff,
-            0 0 70px #ffffff;
-    }
-    to {
-        text-shadow: 0 0 10px #fff, 0 0 3px #fff, 0 0 3px #ffffff, 0 0 3px #ffffff, 0 0 1px #ffffff, 0 0 1px #ffffff,
-            0 0 70px #ffffff;
-    }
+  from {
+    text-shadow: 0 0 10px #fff, 0 0 3px #fff, 0 0 3px #ffffff, 0 0 3px #ffffff,
+      0 0 3px #ffffff, 0 0 10px #ffffff, 0 0 30px #ffffff;
+  }
+  to {
+    text-shadow: 0 0 10px #fff, 0 0 3px #fff, 0 0 3px #ffffff, 0 0 3px #ffffff,
+      0 0 1px #ffffff, 0 0 1px #ffffff, 0 0 70px #ffffff;
+  }
 }
 </style>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -15,37 +15,5 @@ useHead({
       <slot />
     </div>
 
-    <div
-      class="fixed bottom-0 left-1/2 -translate-x-1/2 p-4 z-50 flex items-center space-x-4"
-    >
-      <NuxtLink
-        to="/first"
-        class="relative w-40 h-20 rounded-md overflow-hidden opacity-35 hover:opacity-100 group"
-      >
-        <img
-          src="/images/augustamobile.webp"
-          class="w-full h-full object-cover transform group-hover:scale-110 transition-transform duration-300 ease-in-out"
-          alt="Augusta"
-        />
-        <div
-          class="absolute inset-0 flex items-center justify-center text-white text-xl font-semibold pointer-events-none"
-        >
-        </div>
-      </NuxtLink>
-      <NuxtLink
-        to="/second"
-        class="relative w-40 h-20 rounded-md overflow-hidden opacity-35 hover:opacity-100 group"
-      >
-        <img
-          src="/images/iuno-mobile.webp"
-          class="w-full h-full object-cover transform group-hover:scale-110 transition-transform duration-300 ease-in-out"
-          alt="Iuno"
-        />
-        <div
-          class="absolute inset-0 flex items-center justify-center text-white text-xl font-semibold pointer-events-none"
-        >
-        </div>
-      </NuxtLink>
-    </div>
   </main>
 </template>
