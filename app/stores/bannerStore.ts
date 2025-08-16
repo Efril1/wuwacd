8300e394f2e33e2cd53adbe8699a4e399b098a67
Efril1 <ccenkcc@gmail.com>
2025-08-16 21:25:33 +0300

    fix

--- app/stores/bannerStore.ts
+++ app/stores/bannerStore.ts
@@ -1,35 +1,35 @@
-type ColorChoice = 
+type ColorChoice =
   | "primary"
   | "info"
   | "secondary"
   | "success"
   | "warning"
   | "error"
-  | "neutral"
+  | "neutral";
 
 interface CharacterInfo {
-  name: string
-  element: string
-  weapon: string
-  about: string
+  name: string;
+  element: string;
+  weapon: string;
+  about: string;
 }
 
 interface Banner {
-  targetTime: Date
-  bannerChar: string
-  infoImageDesktop: string
-  infoImageMobile: string
-  infoImageMobile1: string
-  infoImageMobile2: string
-  backgroundVideo: string
-  colorChoice: ColorChoice
-  characterInfo: CharacterInfo
+  targetTime: Date;
+  bannerChar: string;
+  infoImageDesktop: string;
+  infoImageMobile: string;
+  infoImageMobile1: string;
+  infoImageMobile2: string;
+  backgroundVideo: string;
+  colorChoice: ColorChoice;
+  characterInfo: CharacterInfo;
 }
 
-export const useBannerStore = defineStore('banner', () => {
+export const useBannerStore = defineStore("banner", () => {
   const banners = ref<Banner[]>([
     {
-      targetTime: new Date(Date.UTC(2025, 7, 28, 3, 0, 0)), 
+      targetTime: new Date(Date.UTC(2025, 7, 28, 3, 0, 0)),
       bannerChar: "AUGUSTA",
       infoImageDesktop: "/images/augustacard.webp",
       infoImageMobile: "/images/augustamobile.webp",
@@ -41,34 +41,34 @@ export const useBannerStore = defineStore('banner', () => {
         name: "Augusta",
         element: "Electro",
         weapon: "Broadblade",
-        about: '"All the sun touches is ours to fight for."—Augusta'
-      }
+        about: '"All the sun touches is ours to fight for."—Augusta',
+      },
     },
 
     {
-      targetTime: new Date(Date.UTC(2025, 8, 17, 3, 0, 0)), 
+      targetTime: new Date(Date.UTC(2025, 8, 17, 3, 0, 0)),
       bannerChar: "IUNO",
       infoImageDesktop: "/images/Iuno_Card.webp",
       infoImageMobile: "/images/iuno-mobile.webp",
       infoImageMobile1: "/images/augustamobile.webp",
       infoImageMobile2: "/images/iuno-mobile.webp",
       backgroundVideo: "/images/iunovid.mp4",
-      colorChoice:"secondary",
+      colorChoice: "secondary",
       characterInfo: {
         name: "Iuno",
         element: "Aero",
         weapon: "Gauntlet",
-        about: '"While the moon cycles, I remain the anchor."—Iuno'
-      }
-    }
-  ])
+        about: '"While the moon cycles, I remain the anchor."—Iuno',
+      },
+    },
+  ]);
 
   const getBanner = (index: number) => {
-    return banners.value[index] || banners.value[0]
-  }
+    return banners.value[index] || banners.value[0];
+  };
 
   return {
     banners,
-    getBanner
-  }
-})
+    getBanner,
+  };
+});
